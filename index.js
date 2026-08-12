9275bb89f78c0f84ebc4fb8d7b91d85e06168a08
imran4login <imran4login@gmail.com>
2026-08-12 09:41:49 +0300

    UPD-6

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from 'express'
 const app = express();
 const PORT = process.env.PORT || 8080;
-const UPD = 'UPD-5-Srvr';
+const DIR = 'v7-git-actions-12ag26';
+const UPD = 'UPD-6-Srvr';
 
 
 app.get('/', (req, res) => {
-    return res.send(`Hello from Server - ${PORT} - ${UPD}`)
+    return res.send(`Hello from Server - ${PORT} - ${DIR} - ${UPD}`)
 })
 
 
 app.listen(PORT, () => {
-    console.log(`Server is up & running on port ${PORT} - ${UPD}`)
+    console.log(`Server is up & running on port ${PORT} - ${DIR} - ${UPD}`)
 })
 
 
